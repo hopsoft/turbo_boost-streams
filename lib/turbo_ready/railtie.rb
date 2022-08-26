095d6a7077152a1d3286768bc930b2d1113541cd
Stephen Margheim <stephen.margheim@gmail.com>
2022-08-26 20:23:39 +0200

    Add TurboReady stream actions to Turbo in a gem initializer (#1)

--- lib/turbo_ready/railtie.rb
+++ lib/turbo_ready/railtie.rb
@@ -2,5 +2,11 @@
 
 module TurboReady
   class Railtie < ::Rails::Railtie
+    initializer "turbo_ready.patch" do
+      ::ActiveSupport.on_load(:action_cable) do
+        # Adds TurboReady stream actions to Turbo
+        TurboReady.patch!
+      end
+    end
   end
 end
