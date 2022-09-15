2230d7d26cbc59aa25d18d67b20b1be80c40f0b2
Nate Hopkins (hopsoft) <natehop@gmail.com>
2022-09-15 17:45:13 -0600

    Get first demo structured and working.

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get "frames/:id/:behavior", to: "frames#show", as: :frame
   resources :demos, only: %i[index show update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
