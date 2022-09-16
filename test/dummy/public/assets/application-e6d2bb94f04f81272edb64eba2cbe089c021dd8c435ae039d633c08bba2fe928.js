94e0f921c5c8cb0db6a8fd243dda853d435bba84
Nate Hopkins (hopsoft) <natehop@gmail.com>
2022-09-16 17:40:12 -0600

    Recompile assets

--- test/dummy/public/assets/application-e6d2bb94f04f81272edb64eba2cbe089c021dd8c435ae039d633c08bba2fe928.js
+++
@@ -1,9 +0,0 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-
-import '@hotwired/turbo-rails'
-import TurboReady from 'turbo_ready'
-import './demos/event'
-
-TurboReady.initialize(Turbo.StreamActions)
-
-window.TurboReady = TurboReady;
