9f692a4b892e325cb78e0d1f332518c48e4c4bac
Nate Hopkins (hopsoft) <natehop@gmail.com>
2022-09-16 18:33:44 -0600

    Delete assets

--- test/dummy/public/assets/demos/event-2789fdb435598fe5ac25485e32da346962783fa0d36d3b206c495a5b7c77aaee.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('turbo-ready:demo', event => {
-  if (event.target !== window) return
-  console.log('received the turbo-ready:demo event', event.target, event)
-})
-
-document.addEventListener('turbo-ready:demo', event => {
-  console.log('received the turbo-ready:demo event', event.target, event)
-});
--- test/dummy/public/assets/turbo_ready-16217e34c7e57b388c71d9fcd3296f217b371a05314222dde927b4fbc5d1f052.js
+++
@@ -1,4 +0,0 @@
-function f(i,a,o={}){let r=new CustomEvent(a,o);i.forEach(t=>t.dispatchEvent(r))}function h(){let i=JSON.parse(this.templateContent.textContent),{id:a,selector:o,receiver:r,method:t,args:c}=i,e=[self];if(o&&(e=Array.from(document.querySelectorAll(o))),r&&(e=e.map(n=>{let s=n,l=r.split(".");for(;l.length>0;)s=s[l.shift()];return s})),t==="dispatchEvent")return f(e,c[0],c[1]||{});if(t.endsWith("="))return e.forEach(n=>n[t.slice(0,-1).trim()]=c[0]);e.forEach(n=>n[t].apply(n,c))}function p(i){i.invoke=h}var u={initialize:p};export{u as default};
-//# sourceMappingURL=/assets/turbo_ready.js-b25a46777337e5d9c351240f36123cd7c83bd6601cc98e4d5f8a546699ab3c93.map
-//!
-;
