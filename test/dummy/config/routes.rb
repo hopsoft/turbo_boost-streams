18a1bfa1a13e62d203fc93434ecf4e994e9ec888
Nate Hopkins (hopsoft) <natehop@gmail.com>
2022-09-19 03:52:09 -0600

    Move caution page to docs... minor refactors

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -3,8 +3,6 @@
 Rails.application.routes.draw do
   get "frames/:id/:behavior", to: "frames#show", as: :frame
   resources :demos, only: %i[index show update]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  resources :docs, only: %i[show]
   root "demos#index"
 end
