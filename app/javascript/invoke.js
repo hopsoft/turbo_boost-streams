1ed832ddf957124577152db317711931864546d3
Nate Hopkins (hopsoft) <natehop@gmail.com>
2022-10-12 14:32:27 -0600

    Small refactor to reduce method complexity

--- app/javascript/invoke.js
+++ app/javascript/invoke.js
@@ -1,6 +1,22 @@
 import dispatch from './methods/dispatch'
 import morph from './methods/morph'
 
+function perform (method, args, receivers) {
+  // dispatchEvent
+  if (method === 'dispatchEvent')
+    return dispatch(receivers, args[0], args[1] || {})
+
+  // morph
+  if (method === 'morph') return morph(receivers, args[0])
+
+  // property assignment
+  if (method.endsWith('='))
+    return receivers.forEach(r => (r[method.slice(0, -1).trim()] = args[0]))
+
+  // method invocation
+  receivers.forEach(r => r[method].apply(r, args))
+}
+
 function invoke () {
   const payload = JSON.parse(this.templateContent.textContent)
   const { id, selector, receiver, method, args } = payload
@@ -16,19 +32,7 @@ function invoke () {
     })
   }
 
-  // event dispatch
-  if (method === 'dispatchEvent')
-    return dispatch(receivers, args[0], args[1] || {})
-
-  // morph
-  if (method === 'morph') return morph(receivers, args[0])
-
-  // property assignment
-  if (method.endsWith('='))
-    return receivers.forEach(r => (r[method.slice(0, -1).trim()] = args[0]))
-
-  // method invocation
-  receivers.forEach(r => r[method].apply(r, args))
+  perform(method, args, receivers)
 }
 
 export default invoke
