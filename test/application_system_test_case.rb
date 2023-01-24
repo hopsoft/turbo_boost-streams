6fe95619c4e940e38c27b81704280607ff594012
Nate Hopkins <natehop@gmail.com>
2023-01-24 09:34:44 -0700

    Port system tests to Playwright (#40)

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,22 +1,55 @@
 # frozen_string_literal: true
 
 require "test_helper"
-require "capybara/cuprite"
+require "capybara-playwright-driver"
 
-# Capybara setup fom Evil Martians
-# SEE: https://evilmartians.com/chronicles/system-of-a-test-setting-up-end-to-end-rails-testing
-# NOTE: Will need to set this up for multiple sessions
-Capybara.default_max_wait_time = 5
+class CapybaraNullDriver < Capybara::Driver::Base
+  def needs_server?
+    true
+  end
+end
+
+Capybara.register_driver(:null) { CapybaraNullDriver.new }
+Capybara.default_driver = :null
+Capybara.default_max_wait_time = 12
 Capybara.default_normalize_ws = true
-# Capybara.save_path = ENV.fetch("CAPYBARA_ARTIFACTS", "./tmp/capybara")
+Capybara.save_path = "tmp/capybara"
+Capybara.configure do |config|
+  config.server = :puma, {Silent: true}
+end
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :cuprite, using: :headless_chrome, screen_size: [1400, 1400]
+  driven_by :null
+
+  def self.playwright
+    @playwright ||= Playwright.create(playwright_cli_executable_path: Rails.root.join("../../node_modules/.bin/playwright"))
+  end
+
+  alias_method :orig_page, :page
+  attr_reader :playwright_browser, :playwright_page
+  alias_method :page, :playwright_page
+
+  def js(...)
+    page.evaluate(...)
+  end
+
+  def before_setup
+    super
+    base_url = Capybara.current_session.server.base_url
+    @playwright_browser = self.class.playwright.playwright.chromium.launch(headless: true)
+    @playwright_page = @playwright_browser.new_page(baseURL: base_url)
+    playwright_page.set_default_timeout Capybara.default_max_wait_time * 1_000
+  end
+
+  def after_teardown
+    super
+    playwright_browser&.close
+  end
 
   protected
 
   def assert_event_dispatch(wait: 0, invocations: 1)
-    page.execute_script <<~JS
+    js <<~JS
       self.dispatchedEvents = []
       document.addEventListener('turbo-boost:stream:before-invoke', event => self.dispatchedEvents.push(event.type))
       document.addEventListener('turbo-boost:stream:after-invoke', event => self.dispatchedEvents.push(event.type))
@@ -24,16 +57,17 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
     JS
 
     yield
+    sleep 0.1 # wait for turbo streams to execute
 
-    assert page.evaluate_script("self.dispatchedEvents.includes('turbo-boost:stream:before-invoke')")
-    assert page.evaluate_script("self.dispatchedEvents.includes('turbo-boost:stream:after-invoke')")
-    sleep wait if wait > 0
-    assert page.evaluate_script("self.dispatchedEvents.includes('turbo-boost:stream:finish-invoke')")
+    assert js("self.dispatchedEvents.includes('turbo-boost:stream:before-invoke')")
+    assert js("self.dispatchedEvents.includes('turbo-boost:stream:after-invoke')")
+    sleep wait if wait > 0 # wait for promise to resolve if invoked method returns a promise
+    assert js("self.dispatchedEvents.includes('turbo-boost:stream:finish-invoke')")
 
     return if ENV["GITHUB_ACTIONS"].present?
 
     # This check doesn't work with GitHub Actions for some reason
-    event_count = page.evaluate_script("self.dispatchedEvents.length")
+    event_count = js("self.dispatchedEvents.length")
     assert_equal invocations * 3, event_count
   end
 end
--- test/system/demos/method_test.rb
+++ test/system/demos/method_test.rb
@@ -5,58 +5,60 @@ require "application_system_test_case"
 module TurboBoost::Streams::Demos
   class MethodTest < ApplicationSystemTestCase
     test "alert" do
-      visit demo_url("method")
+      page.goto demo_url("method")
 
-      demo_button = find("turbo-frame[id=alert-demo-button] button")
+      demo_element = page.wait_for_selector("turbo-frame[id=alert-demo-button]")
 
-      accept_alert text: "Hello from TurboBoost Streams!" do
-        demo_button.click
+      message = page.capybara_accept_modal "alert" do
+        demo_element.wait_for_selector("button").click
       end
+
+      assert_equal "Hello from TurboBoost Streams!", message
     end
 
     test "alert with delay" do
-      visit demo_url("method")
+      page.goto demo_url("method")
 
-      demo_button = find("turbo-frame[id=alert-demo-button-with-delay] button")
+      demo_element = page.wait_for_selector("turbo-frame[id=alert-demo-button-with-delay]")
 
-      accept_alert text: "Hello from TurboBoost Streams with a delay!" do
-        demo_button.click
+      message = page.capybara_accept_modal "alert", wait: 2.1 do
+        demo_element.wait_for_selector("button").click
       end
+
+      assert_equal "Hello from TurboBoost Streams with a delay!", message
     end
 
     test "events with animation and css selector" do
-      visit demo_url("method")
+      page.goto demo_url("method")
 
-      demo_button = find("turbo-frame[id=animate-demo-button] button")
+      demo_element = page.wait_for_selector("turbo-frame[id=animate-demo-button]")
 
       assert_event_dispatch wait: 3 do
-        demo_button.click
+        demo_element.wait_for_selector("button").click
       end
     end
 
     test "select multiple elements" do
-      visit demo_url("method")
+      page.goto demo_url("method")
 
-      demo_button = find("turbo-frame[id=multiple-elements-demo-button] button")
+      demo_element = page.wait_for_selector("turbo-frame[id=multiple-elements-demo-button]")
+      button_elements = page.query_selector_all("button[type=submit]")
 
-      submit_buttons = find_all("button[type=submit]")
       expected_css_classes = %w[bg-gradient-to-br dark:focus:ring-pink-800 focus:ring-pink-200 from-pink-500 to-orange-400]
-      submit_buttons.each do |button|
-        expected_css_classes.each do |css_class|
-          assert_equal expected_css_classes.size, (button[:class].split(" ") & expected_css_classes).size
-        end
+      button_elements.each do |button|
+        assert_equal expected_css_classes.size, (button["class"].split(" ") & expected_css_classes).size
       end
 
       assert_event_dispatch invocations: 10, wait: 1 do
-        demo_button.click
+        demo_element.wait_for_selector("button").click
       end
 
-      submit_buttons = find_all("button[type=submit]")
+      demo_element.wait_for_selector("button[type=submit][class~=from-cyan-500]")
+      button_elements = page.query_selector_all("button[type=submit]")
+
       expected_css_classes = %w[bg-gradient-to-r dark:focus:ring-cyan-800 focus:ring-cyan-300 from-cyan-500 to-blue-500]
-      submit_buttons.each do |button|
-        expected_css_classes.each do |css_class|
-          assert_equal expected_css_classes.size, (button[:class].split(" ") & expected_css_classes).size
-        end
+      button_elements.each do |button|
+        assert_equal expected_css_classes.size, (button["class"].split(" ") & expected_css_classes).size
       end
     end
   end
