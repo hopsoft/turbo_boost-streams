5276495f9058907986d40203645850b4a375cf84
Nate Hopkins (hopsoft) <natehop@gmail.com>
2023-05-04 13:48:03 -0600

    Ensure initial clone & startup facilitates contributions

--- Dockerfile
+++ Dockerfile
@@ -8,12 +8,14 @@ libjemalloc2 \
 sqlite3 \
 tzdata
 
+
 RUN curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
-RUN apt-get -y --no-install-recommends install nodejs
+RUN apt-get -y --no-install-recommends install nodejs && \
+npm install -g npm@latest yarn
+
 RUN apt-get clean
 RUN gem update --system && gem install bundler
-RUN npm install -g yarn
 
 COPY . /opt/turbo_boost-streams
 
-CMD /opt/turbo_boost-streams/bin/docker/run
+CMD /opt/turbo_boost-streams/bin/docker/run/remote
--- test/dummy/config/importmap.rb
+++ test/dummy/config/importmap.rb
@@ -8,13 +8,8 @@ pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@7
 pin "@rails/actioncable/src", to: "https://ga.jspm.io/npm:@rails/actioncable@7.0.4/src/index.js"
 pin "flowbite", to: "https://ga.jspm.io/npm:flowbite@1.5.3/dist/flowbite.js"
 
-# this pin works because of the link_tree directive in: test/dummy/app/assets/config/manifest.js
-# that points to the relative path of the build directory
-# SEE: package.json for details on the build script
-# pin "@turbo-boost/streams", to: "@turbo-boost/streams.js"
-
 FileUtils.rm_f Rails.root.join("app/javascript/@turbo-boost")
 FileUtils.ln_s Rails.root.join("../../app/assets/builds/@turbo-boost"), Rails.root.join("app/javascript/@turbo-boost")
 pin "@turbo-boost/streams", to: "@turbo-boost/streams.js"
 
-pin "application" # , preload: true
+pin "application", preload: true
