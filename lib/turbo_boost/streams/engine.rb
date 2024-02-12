9f6c1cc34c49615727aed28176a7d8539f86ca8e
Mikael Henriksson <mikael@mhenrixon.com>
2024-02-12 22:06:58 +0200

    fix(js): allow pinning importmaps with gem (#51)
    Since import maps JSPM and the javascript ecosystem are a mess in their current state, allow import maps to match the assets served by the gem rather than download them from JSPM.

--- lib/turbo_boost/streams/engine.rb
+++ lib/turbo_boost/streams/engine.rb
@@ -5,9 +5,20 @@ require_relative "version"
 require_relative "patches"
 
 class TurboBoost::Engine < ::Rails::Engine
+  config.turbo_boost_streams = ActiveSupport::OrderedOptions.new
+  config.turbo_boost_streams.precompile_assets = true
+
   config.after_initialize do
     ::Turbo::Streams::TagBuilder.send :include, TurboBoost::Streams::Patches::TagBuilder
     ::Turbo::Streams::Broadcasts.send :include, TurboBoost::Streams::Patches::Broadcasts
     ::Turbo::Broadcastable.send :include, TurboBoost::Streams::Patches::Broadcastable
+  end
+
+  initializer "turbo_boost_streams.asset" do
+    config.after_initialize do |app|
+      if app.config.respond_to?(:assets) && app.config.turbo_boost_streams.precompile_assets
+        app.config.assets.precompile += %w[@turbo-boost/streams.js]
+      end
+    end
   end
 end
